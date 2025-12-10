ad5218f9b1fda45310069650d0ab41951c327f4b
koonerjatindersingh-art <koonerjatindersingh@gmail.com>
2025-12-10 10:43:58 +0530

    Add files via upload

---
+++ main/management/commands/render_migrate.py
@@ -0,0 +1,7 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        call_command("makemigrations")
+        call_command("migrate")
