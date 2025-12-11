8c599e96c9cc13b10b2ea9633f0decf70e78d643
koonerjatindersingh-art <koonerjatindersingh@gmail.com>
2025-12-11 15:15:00 +0530

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    main.apps.MainConfig',
+   'main.apps.MainConfig', 
 ]
 
 MIDDLEWARE = [
@@ -132,6 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
